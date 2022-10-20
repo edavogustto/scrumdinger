e24260c645fcf798bd97a4060bd3c11a0e4f88e7
edavogustto <ernesto.daogustto@gmail.com>
2022-10-19 22:53:43 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ernesto Davogustto on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
