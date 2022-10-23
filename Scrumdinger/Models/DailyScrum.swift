2665f40707de808fb1c5630f0288f7717b16ac1f
edavogustto <ernesto.daogustto@gmail.com>
2022-10-22 23:02:58 -0300

    Making scrums identifiable

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,18 +7,28 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable{
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init (id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
-    static let  sampleData: [DailyScrum] =
-    [
-        DailyScrum(title: "Desing", attendees: ["Carla", "Sophia", "Maria", "Simon"], lengthInMinutes: 10, theme: .poppy),
-        DailyScrum(title: "Web Dev", attendees: ["Mateo", "Jhon", "Ryan"], lengthInMinutes: 15, theme: .bubblegum),
-        DailyScrum(title: "iOS Dev", attendees: ["Dwayne", "Bradley", "Benito", "Fernanda", "Lindsey", "Chad"], lengthInMinutes: 25, theme: .buttercup),
-    ]
+    static var sampleData: [DailyScrum] {
+        [
+            DailyScrum(title: "Desing", attendees: ["Carla", "Sophia", "Maria", "Simon"], lengthInMinutes: 10, theme: .poppy),
+            DailyScrum(title: "Web Dev", attendees: ["Mateo", "Jhon", "Ryan"], lengthInMinutes: 15, theme: .bubblegum),
+            DailyScrum(title: "iOS Dev", attendees: ["Dwayne", "Bradley", "Benito", "Fernanda", "Lindsey", "Chad"], lengthInMinutes: 25, theme: .buttercup),
+        ]
+    }
 }
